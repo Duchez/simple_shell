b479454e23eb74a59413952541f6c8c9da7b760d
Duchez <edukoo@gmail.com>
2020-11-24 17:42:15 +0300

    headers

---
+++ commands.h
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+int exits(char **ptr);
+int cdir(char **ptr);
+
+
+
+
+
+
+
+
+
+#endif /*COMMANDS_H*/
---
+++ exits.c
@@ -0,0 +1,12 @@
+#include "commands.h"
+#include "shell.h"
+/**
+ * exits - closes the shell
+ * @ptr: pointer
+ * Return: 0 on success
+ */
+
+int exits(char)
+{
+        return (0);
+}
